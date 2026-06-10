0f92acb264b2b55f6848ad6d6a743636ff2f77c3
Fabian <s255337@dtu.dk>
2026-06-10 15:19:28 +0200

    Update GameLogic.scala

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -18,8 +18,8 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
     val spriteFlipHorizontal = Output(Vec(SpriteNumber, Bool()))
     val spriteFlipVertical = Output(Vec(SpriteNumber, Bool()))
 
-    val viewBoxX = Output(UInt(10.W))
-    val viewBoxY = Output(UInt(9.W))
+    val viewBoxX = Output(UInt(11.W))
+    val viewBoxY = Output(UInt(10.W))
 
     val backBufferWriteData = Output(UInt(log2Up(BackTileNumber).W))
     val backBufferWriteAddress = Output(UInt(11.W))
@@ -36,9 +36,6 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
   io.spriteVisible := Seq.fill(SpriteNumber)(false.B)
   io.spriteFlipHorizontal := Seq.fill(SpriteNumber)(false.B)
   io.spriteFlipVertical := Seq.fill(SpriteNumber)(false.B)
-
-  io.viewBoxX := 0.U
-  io.viewBoxY := 0.U
 
   io.backBufferWriteData := 0.U
   io.backBufferWriteAddress := 0.U
@@ -67,53 +64,46 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
   io.spriteFlipVertical(1) := car.io.flipV
   io.spriteFlipVertical(2) := car.io.flipV
 
-  io.spriteXPosition(0) := car.io.posX
-  io.spriteXPosition(1) := car.io.posX
-  io.spriteXPosition(2) := car.io.posX
+  // Camera Follow with map bounds
 
-  io.spriteYPosition(0) := car.io.posY
-  io.spriteYPosition(1) := car.io.posY
-  io.spriteYPosition(2) := car.io.posY
+  val cameraX = Wire(SInt(11.W))
+  val cameraY = Wire(SInt(10.W))
+
+  val desiredCameraX = car.io.posX - 304.S
+  val desiredCameraY = car.io.posY - 224.S
+
+  when(desiredCameraX < 0.S) {
+    cameraX := 0.S
+  }.elsewhen(desiredCameraX > 640.S) {
+    cameraX := 640.S
+  }.otherwise {
+    cameraX := desiredCameraX
+  }
+
+  when(desiredCameraY < 0.S) {
+    cameraY := 0.S
+  }.elsewhen(desiredCameraY > 480.S) {
+    cameraY := 480.S
+  }.otherwise {
+    cameraY := desiredCameraY
+  }
+
+  io.viewBoxX := cameraX.asUInt
+  io.viewBoxY := cameraY.asUInt
+
+  io.spriteXPosition(0) := car.io.posX - cameraX
+  io.spriteXPosition(1) := car.io.posX - cameraX
+  io.spriteXPosition(2) := car.io.posX - cameraX
+
+  io.spriteYPosition(0) := car.io.posY - cameraY
+  io.spriteYPosition(1) := car.io.posY - cameraY
+  io.spriteYPosition(2) := car.io.posY - cameraY
 
   io.spriteVisible(0) := car.io.shownSprite(0)
   io.spriteVisible(1) := car.io.shownSprite(1)
   io.spriteVisible(2) := car.io.shownSprite(2)
 
   car.io.update := false.B
-
-  // Camera Follow
-
-  val WORLD_WIDTH  = 1280.S(12.W)
-  val WORLD_HEIGHT = 960.S(11.W)
-
-  val SCREEN_WIDTH  = 640.S(12.W)
-  val SCREEN_HEIGHT = 480.S(11.W)
-
-  val CAR_WIDTH  = 32.S(6.W)
-  val CAR_HEIGHT = 32.S(6.W)
-
-  val desiredViewX =
-  car.io.posX - (SCREEN_WIDTH / 2.S) + 16.S
-
-  val desiredViewY =
-  car.io.posY - (SCREEN_HEIGHT / 2.S) + 16.S
-
-  when(desiredViewX < 0.S) {
-    io.viewBoxX := 0.U
-  }.elsewhen(desiredViewX > (WORLD_WIDTH - SCREEN_WIDTH)) {
-    io.viewBoxX := (WORLD_WIDTH - SCREEN_WIDTH).asUInt
-  }.otherwise {
-    io.viewBoxX := desiredViewX.asUInt
-  }
-
-  when(desiredViewY < 0.S) {
-    io.viewBoxY := 0.U
-  }.elsewhen(desiredViewY > (WORLD_HEIGHT - SCREEN_HEIGHT)) {
-    io.viewBoxY := (WORLD_HEIGHT - SCREEN_HEIGHT).asUInt
-  }.otherwise {
-    io.viewBoxY := desiredViewY.asUInt
-  }
-
 
   // FSM
 
