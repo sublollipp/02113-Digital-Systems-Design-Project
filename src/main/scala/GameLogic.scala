7ef381209d018e303d4c1cc4f904595a8d981cd2
Fabian <s255337@dtu.dk>
2026-06-10 14:11:10 +0200

    Update GameLogic.scala

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -1,93 +1,53 @@
-//////////////////////////////////////////////////////////////////////////////
-// Authors: Luca Pezzarossa
-// Copyright: Technical University of Denmark - 2025
-// Comments:
-// This file contains the game logic. Implement yours here.
-//////////////////////////////////////////////////////////////////////////////
-
 import chisel3._
 import chisel3.util._
 
 class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
   val io = IO(new Bundle {
-    //Buttons
     val btnC = Input(Bool())
     val btnU = Input(Bool())
     val btnL = Input(Bool())
     val btnR = Input(Bool())
     val btnD = Input(Bool())
 
-    //Switches
     val sw = Input(Vec(8, Bool()))
-
-    //Leds
     val led = Output(Vec(8, Bool()))
 
-    //GraphicEngineVGA
-    //Sprite control input
-    val spriteXPosition = Output(Vec(SpriteNumber, SInt(11.W))) //-1024 to 1023
-    val spriteYPosition = Output(Vec(SpriteNumber, SInt(10.W))) //-512 to 511
+    val spriteXPosition = Output(Vec(SpriteNumber, SInt(11.W)))
+    val spriteYPosition = Output(Vec(SpriteNumber, SInt(10.W)))
     val spriteVisible = Output(Vec(SpriteNumber, Bool()))
     val spriteFlipHorizontal = Output(Vec(SpriteNumber, Bool()))
     val spriteFlipVertical = Output(Vec(SpriteNumber, Bool()))
 
-    //Viewbox control output
-    val viewBoxX = Output(UInt(10.W)) //0 to 640
-    val viewBoxY = Output(UInt(9.W)) //0 to 480
+    val viewBoxX = Output(UInt(10.W))
+    val viewBoxY = Output(UInt(9.W))
 
-    //Background buffer output
     val backBufferWriteData = Output(UInt(log2Up(BackTileNumber).W))
     val backBufferWriteAddress = Output(UInt(11.W))
     val backBufferWriteEnable = Output(Bool())
 
-    //Status
     val newFrame = Input(Bool())
     val frameUpdateDone = Output(Bool())
   })
 
-  // Setting all led outputs to zero
-  // It can be done by the single expression below...
   io.led := Seq.fill(8)(false.B)
 
-  // Or one by one...
-  //io.led(0) := false.B
-  //io.led(0) := false.B
-  //io.led(1) := false.B
-  //io.led(2) := false.B
-  //io.led(3) := false.B
-  //io.led(4) := false.B
-  //io.led(5) := false.B
-  //io.led(6) := false.B
-  //io.led(7) := false.B
-
-  // Or with a for loop.
-  //for (i <- 0 until 8) {
-  //  io.led(i) := false.B
-  //}
-
-  //Setting all sprite control outputs to zero
   io.spriteXPosition := Seq.fill(SpriteNumber)(0.S)
   io.spriteYPosition := Seq.fill(SpriteNumber)(0.S)
   io.spriteVisible := Seq.fill(SpriteNumber)(false.B)
   io.spriteFlipHorizontal := Seq.fill(SpriteNumber)(false.B)
   io.spriteFlipVertical := Seq.fill(SpriteNumber)(false.B)
 
-  //Setting the viewbox control outputs to zero
   io.viewBoxX := 0.U
   io.viewBoxY := 0.U
 
-  //Setting the background buffer outputs to zero
   io.backBufferWriteData := 0.U
   io.backBufferWriteAddress := 0.U
   io.backBufferWriteEnable := false.B
 
-  //Setting frame done to zero
   io.frameUpdateDone := false.B
 
-  /////////////////////////////////////////////////////////////////
-  // Write here your game logic
-  // (you might need to change the initialization values above)
-  /////////////////////////////////////////////////////////////////
+
+  // Game Logic
 
   val idle :: compute1 :: done :: Nil = Enum(3)
   val stateReg = RegInit(idle)
@@ -98,45 +58,74 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int) extends Module {
   car.io.btnUp := io.btnU
   car.io.btnRight := io.btnR
   car.io.btnDown := io.btnD
+
   io.spriteFlipHorizontal(0) := car.io.flipH
   io.spriteFlipHorizontal(1) := car.io.flipH
   io.spriteFlipHorizontal(2) := car.io.flipH
-  io.spriteFlipVertical(0) := car.io.flipH
-  io.spriteFlipVertical(1) := car.io.flipH
-  io.spriteFlipVertical(2) := car.io.flipH
+
+  io.spriteFlipVertical(0) := car.io.flipV
+  io.spriteFlipVertical(1) := car.io.flipV
+  io.spriteFlipVertical(2) := car.io.flipV
+
   io.spriteXPosition(0) := car.io.posX
   io.spriteXPosition(1) := car.io.posX
   io.spriteXPosition(2) := car.io.posX
+
   io.spriteYPosition(0) := car.io.posY
   io.spriteYPosition(1) := car.io.posY
   io.spriteYPosition(2) := car.io.posY
+
   io.spriteVisible(0) := car.io.shownSprite(0)
   io.spriteVisible(1) := car.io.shownSprite(1)
   io.spriteVisible(2) := car.io.shownSprite(2)
+
   car.io.update := false.B
 
-  switch (stateReg) {
-    is (idle) {
-      when (io.newFrame) {
+  // Camera Follow
+
+  val WORLD_WIDTH  = 1280.S
+  val WORLD_HEIGHT = 960.S
+
+  val SCREEN_WIDTH  = 640.S
+  val SCREEN_HEIGHT = 480.S
+
+  val desiredViewX = car.io.posX - (SCREEN_WIDTH / 2.S)
+  val desiredViewY = car.io.posY - (SCREEN_HEIGHT / 2.S)
+
+  when(desiredViewX < 0.S) {
+    io.viewBoxX := 0.U
+  }.elsewhen(desiredViewX > (WORLD_WIDTH - SCREEN_WIDTH)) {
+    io.viewBoxX := (WORLD_WIDTH - SCREEN_WIDTH).asUInt
+  }.otherwise {
+    io.viewBoxX := desiredViewX.asUInt
+  }
+
+  when(desiredViewY < 0.S) {
+    io.viewBoxY := 0.U
+  }.elsewhen(desiredViewY > (WORLD_HEIGHT - SCREEN_HEIGHT)) {
+    io.viewBoxY := (WORLD_HEIGHT - SCREEN_HEIGHT).asUInt
+  }.otherwise {
+    io.viewBoxY := desiredViewY.asUInt
+  }
+
+
+  // FSM
+
+  switch(stateReg) {
+    is(idle) {
+      when(io.newFrame) {
         stateReg := compute1
       }
     }
-    is (compute1) {
+
+    is(compute1) {
       car.io.update := true.B
       stateReg := done
     }
+
     is(done) {
       io.frameUpdateDone := true.B
       stateReg := idle
     }
   }
-
-  // Just forwarding the newFrame into the frameUpdateDone with a 2 clock cycle delay
-  // frameUpdateDone will need to be driven by your game logic FSMs
-  io.frameUpdateDone := RegNext(RegNext(io.newFrame))
-
 }
-
-//////////////////////////////////////////////////////////////////////////////
-// End of file
-//////////////////////////////////////////////////////////////////////////////
