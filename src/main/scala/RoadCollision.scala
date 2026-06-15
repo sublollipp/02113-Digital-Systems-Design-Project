8c954fe9f068830eb59cf4f2fa91aaccc6857bdd
Fabian <s255337@dtu.dk>
2026-06-15 10:49:15 +0200

    offroad commit

---
+++ src/main/scala/RoadCollision.scala
@@ -0,0 +1,48 @@
+import chisel3._
+import chisel3.util._
+
+class RoadCollision extends Module {
+  val io = IO(new Bundle {
+    val x = Input(SInt(12.W))
+    val y = Input(SInt(11.W))
+    val onRoad = Output(Bool())
+  })
+
+  // Definér vejstykker her
+  val road1 =
+    io.x >= 96.S &&
+    io.x <= 224.S &&
+    io.y >= 128.S &&
+    io.y <= 864.S
+
+  val road2 =
+    io.x >= 96.S &&
+    io.x <= 1120.S &&
+    io.y >= 128.S &&
+    io.y <= 258.S
+
+  val road3 =
+    io.x >= 992.S &&
+    io.x <= 1120.S &&
+    io.y >= 258.S &&
+    io.y <= 576.S
+
+  val road4 =
+    io.x >= 832.S &&
+    io.x <= 992.S &&
+    io.y >= 416.S &&
+    io.y <= 736.S
+
+  val road5 =
+    io.x >= 96.S &&
+    io.x <= 992.S &&
+    io.y >= 736.S &&
+    io.y <= 864.S
+
+  io.onRoad :=
+    road1 ||
+    road2 ||
+    road3 ||
+    road4 ||
+    road5
+}
