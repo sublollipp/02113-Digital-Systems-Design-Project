0297f71e6a6902b5845758e12d4f7e14b1dab007
Fabian <s255337@dtu.dk>
2026-06-15 16:28:33 +0200

    Update WinCondition.scala

--- src/main/scala/WinCondition.scala
+++ src/main/scala/WinCondition.scala
@@ -9,12 +9,39 @@ class WinCondition extends Module {
     val gameWon = Output(Bool())
   })
 
-  // Målområde (ret selv koordinaterne)
+  // Checkpoint for goal-line
+
+  val checkpointPassed = RegInit(false.B)
+  val lapCounter = RegInit(0.U(3.W))
+
+  val checkpointArea =
+    io.carX >= 320.S &&
+    io.carX <= 448.S &&
+    io.carY >= 576.S &&
+    io.carY <= 608.S
+
+    val checkpointPrev = RegNext(checkpointArea, false.B)
+
+    when(checkpointArea && !checkpointPrev) {
+    checkpointPassed := true.B
+    }
+
+  // Goal-Line
+
   val finishLine =
-    io.carX >= 96.S &&
+    io.carX >= 96.S && 
     io.carX <= 224.S &&
     io.carY >= 224.S &&
     io.carY <= 240.S
 
-  io.gameWon := finishLine
+    val finishLinePrev = RegNext(finishLine, false.B)
+
+    when(finishLine && !finishLinePrev && checkpointPassed) {
+    checkpointPassed := false.B
+
+    when(lapCounter =/= 3.U) {
+        lapCounter := lapCounter + 1.U
+    }
+    }
+    io.gameWon := (lapCounter === 3.U)
 }
