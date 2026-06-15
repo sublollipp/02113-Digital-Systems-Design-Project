8fc19e06b3047514324735e07e266c8105fb25d1
Fabian <s255337@dtu.dk>
2026-06-15 12:17:06 +0200

    Update RoadCollision.scala

--- src/main/scala/RoadCollision.scala
+++ src/main/scala/RoadCollision.scala
@@ -8,37 +8,42 @@ class RoadCollision extends Module {
     val onRoad = Output(Bool())
   })
 
-  // Definér vejstykker her
+  // Vejen venstre del (lodret)
   val road1 =
     io.x >= 64.S &&
-    io.x <= 224.S &&
+    io.x <= 192.S &&
     io.y >= 128.S &&
     io.y <= 832.S
 
+ // Vejen højre del (vandret)
   val road2 =
     io.x >= 96.S &&
     io.x <= 1120.S &&
     io.y >= 96.S &&
     io.y <= 225.S
 
+ // Vejen øverste del (lodret)
   val road3 =
     io.x >= 992.S &&
     io.x <= 1120.S &&
     io.y >= 225.S &&
     io.y <= 544.S
 
+ // Vejen nederste del (lodret)
   val road4 =
     io.x >= 832.S &&
     io.x <= 960.S &&
     io.y >= 416.S &&
     io.y <= 704.S
 
+ // Vejen nederste del (vandret)
   val road5 =
     io.x >= 96.S &&
     io.x <= 960.S &&
     io.y >= 704.S &&
     io.y <= 832.S
 
+ // Vejen mellem road3 og road4 del (vandret)
   val road6 = 
     io.x >= 832.S &&
     io.x <= 1120.S &&
