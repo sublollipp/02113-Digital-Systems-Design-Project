8aa8207d9ffed4c884fc94767c224be6c36f21b2
knack0 <fvp20042004@gmail.com>
2026-06-16 16:55:02 +0200

    update pocket

--- src/test/scala/RNGTester.scala
+++ src/test/scala/RNGTester.scala
@@ -17,12 +17,15 @@ class RNGTester extends AnyFlatSpec with ChiselScalatestTester {
         dut.clock.setTimeout(0)
         val abc = new scala.util.Random
         val counts: Array[Int] = Array(0, 0, 0, 0)
-        for (i <- 1 to 1000) {
+        for (i <- 1 to 10000) {
           val clockDel = abc.nextInt(100)
           dut.clock.step(clockDel + 1)
           val chosenIdx = dut.io.peek().litValue.toInt
-          if (chosenIdx >= 0 && chosenIdx < counts.length) {
-            counts(Math.log(chosenIdx).toInt) += 1
+          if (chosenIdx >= 0) {
+            counts(util.log2Ceil(chosenIdx).toInt) += 1
+          } else {
+            print("AAAAAH DEN VAR:")
+            print(chosenIdx)
           }
         }
       for(i <- 0 to 3) {
