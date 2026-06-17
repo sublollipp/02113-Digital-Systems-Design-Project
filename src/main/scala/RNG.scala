16e4ac04b7e5f22aad042bd24380f798de6a79fd
knack0 <fvp20042004@gmail.com>
2026-06-17 14:15:12 +0200

    update to rng and pocket

--- src/main/scala/RNG.scala
+++ src/main/scala/RNG.scala
@@ -7,11 +7,15 @@ class RNG extends Module {
   })
 
   val oneHotShifter = RegInit(VecInit(false.B, false.B, false.B, true.B))
+  val next = Wire(Vec(4, Bool()))
+
+  next := VecInit(Seq.fill(4)(false.B))
 
   oneHotShifter(0) := oneHotShifter(3)
   oneHotShifter(1) := oneHotShifter(0)
   oneHotShifter(2) := oneHotShifter(1)
   oneHotShifter(3) := oneHotShifter(2)
 
+  oneHotShifter := next
   io.output := oneHotShifter
 }
