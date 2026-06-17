90b78fbcf7682b820e8a1919aa33a54b89aadbeb
Idax028 <s251236@dtu.dk>
2026-06-17 11:08:26 +0200

    Update Secondrunningsprite.scala

---
+++ src/main/scala/Secondrunningsprite.scala
@@ -0,0 +1,70 @@
+import chisel3._
+import chisel3.util._
+
+class SecondrunningSprite extends Module {
+  val io = IO(new Bundle {
+    val update = Input(Bool())
+    val hit = Input(Bool())
+    val posX = Output(SInt(12.W))
+    val posY = Output(SInt(11.W))
+    val hitboxX = Output(SInt(12.W))
+    val hitboxY = Output(SInt(11.W))
+    val hitboxWidth = Output(UInt(6.W))
+    val hitboxHeight = Output(UInt(6.W))
+    val flipH = Output(Bool())
+    val flipV = Output(Bool())
+    val shownSprite = Output(Vec(4, Bool()))
+  })
+
+  val startX = 60.S(12.W)
+  val targetX = (60 + 32 * 32).S(12.W) // 32 tiles right
+
+  val xPosReg = RegInit(120.S(12.W))
+  val yPosReg = RegInit(220.S(11.W))
+  val movingRight = RegInit(true.B)
+  val hitReg = RegInit(false.B)
+
+  val stopped = io.hit || hitReg
+
+  when(io.update) {
+    when(io.hit) {
+      hitReg := true.B
+    }
+
+    when(!stopped) {
+      when(movingRight) {
+        when(xPosReg < targetX) {
+          xPosReg := xPosReg + 1.S
+        }.otherwise {
+          movingRight := false.B
+        }
+      }.otherwise {
+        when(xPosReg > startX) {
+          xPosReg := xPosReg - 1.S
+        }.otherwise {
+          movingRight := true.B
+        }
+      }
+    }
+  }
+
+  val hitboxOffsetX = 4.S(12.W)
+  val hitboxOffsetY = 4.S(11.W)
+  val hitboxWidthValue = 24.U(6.W)
+  val hitboxHeightValue = 24.U(6.W)
+
+  io.posX := xPosReg
+  io.posY := yPosReg
+  io.hitboxX := xPosReg + hitboxOffsetX
+  io.hitboxY := yPosReg + hitboxOffsetY
+  io.hitboxWidth := hitboxWidthValue
+  io.hitboxHeight := hitboxHeightValue
+
+  io.flipH := !movingRight
+  io.flipV := false.B
+
+  io.shownSprite := Mux(hitReg,
+    VecInit(false.B, false.B, false.B, true.B),
+    VecInit(false.B, false.B, true.B, false.B)
+  )
+}
