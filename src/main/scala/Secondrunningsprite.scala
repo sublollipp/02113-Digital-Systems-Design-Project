ad9632d596b21ce147ffd40e0e22a73dab2c89a9
Idax028 <s251236@dtu.dk>
2026-06-18 10:38:51 +0200

    Update Secondrunningsprite.scala

--- src/main/scala/Secondrunningsprite.scala
+++ src/main/scala/Secondrunningsprite.scala
@@ -24,36 +24,43 @@ class SecondrunningSprite extends Module {
   val movingRight = RegInit(true.B)
   val hitReg = RegInit(false.B)
   val hitboxGone = RegInit(false.B)
+  val hiddenReg = RegInit(false.B)
 
   when(io.update) {
-    when(io.hit && !hitReg) {
+    when(io.hit && !hitReg && !hiddenReg) {
       hitReg := true.B
       hitboxGone := true.B
     }
 
-    when(!hitboxGone) {
-      when(movingRight) {
-        when(xPosReg < targetX) {
-          xPosReg := xPosReg + 1.S
+    when(!hiddenReg) {
+      when(hitboxGone) {
+        when(yPosReg > (-32).S(11.W)) {
+          yPosReg := yPosReg - 1.S
         }.otherwise {
-          movingRight := false.B
+          hiddenReg := true.B
         }
       }.otherwise {
-        when(xPosReg > startX) {
-          xPosReg := xPosReg - 1.S
+        when(movingRight) {
+          when(xPosReg < targetX) {
+            xPosReg := xPosReg + 1.S
+          }.otherwise {
+            movingRight := false.B
+          }
         }.otherwise {
-          movingRight := true.B
+          when(xPosReg > startX) {
+            xPosReg := xPosReg - 1.S
+          }.otherwise {
+            movingRight := true.B
+          }
         }
       }
-    }.otherwise {
-      yPosReg := yPosReg - 1.S
     }
   }
 
   val hitboxOffsetX = 4.S(12.W)
   val hitboxOffsetY = 4.S(11.W)
-  val hitboxWidthValue = Mux(hitboxGone, 0.U(6.W), 24.U(6.W))
-  val hitboxHeightValue = Mux(hitboxGone, 0.U(6.W), 24.U(6.W))
+  val hitboxWidthValue = Mux(hitboxGone, 0.U(6.W), 24.U(6.W)) // If the hitbox is gone, set width to 0, otherwise 24
+  val hitboxHeightValue = Mux(hitboxGone, 0.U(6.W), 24.U(6.W)) // If the hitbox is gone, set height to 0, otherwise 24
 
   io.posX := xPosReg
   io.posY := yPosReg
@@ -65,8 +72,11 @@ class SecondrunningSprite extends Module {
   io.flipH := !movingRight
   io.flipV := false.B
 
-  io.shownSprite := Mux(hitReg,
-    VecInit(false.B, false.B, false.B, true.B),
-    VecInit(false.B, false.B, true.B, false.B)
+  io.shownSprite := Mux(hiddenReg,
+    VecInit(false.B, false.B, false.B, false.B),
+    Mux(hitReg,
+      VecInit(false.B, false.B, false.B, true.B),
+      VecInit(false.B, false.B, true.B, false.B)
+    )
   )
 }
